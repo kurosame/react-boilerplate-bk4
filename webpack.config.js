b030a51f6e51853c5b0335fd694e6814b94e7337
kurosame <kurosame1227@gmail.com>
2019-09-05 17:02:19 +0900

    fix: tslint to eslint

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = (_, argv) => ({
               happyPackMode: true
             }
           },
-          'tslint-loader',
+          'eslint-loader',
           'stylelint-custom-processor-loader'
         ],
         exclude: /node_modules/
